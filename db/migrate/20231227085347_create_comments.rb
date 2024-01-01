827fb1f25350586756e5f2922aad3b3ae1fa7d81
Karan Joshi <Karan190213@gmail.com>
2024-01-01 17:00:16 +0530

    update r spec and migrations

--- db/migrate/20231227085347_create_comments.rb
+++ db/migrate/20231227085347_create_comments.rb
@@ -1,9 +1,11 @@
-# CreateComments migration
+# 20231227085347_create_comments.rb
+
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :post, null: false # Remove `foreign_key: true`
+
       t.text :text
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_27_085348) do
 
   create_table "likes", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -55,6 +53,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_27_085348) do
   end
 
   add_foreign_key "comments", "users"
-  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
 end
