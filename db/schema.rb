ea29bae3b898cd528bc90304dd7a5db56f373acb
Karan Joshi <Karan190213@gmail.com>
2024-01-08 14:37:53 +0530

    Fix migration error: "relation 'comments' already exists"
    Fixed issue where existing "comments" table caused migration error. Rolled back, restored missing files, and re-migrated successfully. Tables and schema now correct.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_27_085348) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_27_085400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,8 +26,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_27_085348) do
 
   create_table "likes", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -52,6 +54,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_27_085348) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
 end
