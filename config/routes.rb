814d2009f32fc6029ba3dc5ca46748615c80c7df
Karan Joshi <Karan190213@gmail.com>
2024-01-11 19:32:31 +0530

    Implemented initial controllers and actions for user and post functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # All users page
+  get '/users', to: 'users#index', as: :users
+
+  # Single user page
+  get '/users/:id', to: 'users#show', as: :user
+
+  # All posts by a given user page
+  get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
+
+  # Single post page
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get '/up', to: 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'users#index'
 end
