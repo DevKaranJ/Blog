c97d92354e1710f9b629b86394239c781902ecc5
Karan Joshi <Karan190213@gmail.com>
2024-01-16 17:44:04 +0530

    user helper pending test passes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def full_name(user)
+    "#{user.first_name} #{user.last_name}"
+  end
+
+  def display_age(user)
+    "Age: #{Time.now.year - user.birthdate.year}"
+  end
+
 end
