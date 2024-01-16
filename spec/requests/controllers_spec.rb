b8ff5fac959496a2c202471c5362a5433898dd87
Karan Joshi <Karan190213@gmail.com>
2024-01-16 17:40:45 +0530

    post helper updated

--- spec/requests/controllers_spec.rb
+++ spec/requests/controllers_spec.rb
@@ -12,15 +12,5 @@ RSpec.describe 'Controllers', type: :request do
         expect(response.body).to include('List of Posts')
       end
     end
-
-    describe 'GET show' do
-      it 'renders the show template' do
-        # Assuming you have a user with id 1 and a post with id 1 in your database
-        get user_post_path(1, 1)
-        expect(response).to have_http_status(:ok)
-        expect(response).to render_template(:show)
-        expect(response.body).to include('Test Post')
-      end
-    end
   end
 end
