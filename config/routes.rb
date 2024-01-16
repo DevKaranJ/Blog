596db97de9be6f298963d84859df99f4d6fd95d4
Karan Joshi <Karan190213@gmail.com>
2024-01-16 12:26:15 +0530

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  # All users page
-  get '/users', to: 'users#index', as: :users
-
-  # Single user page
-  get '/users/:id', to: 'users#show', as: :user
-
-  # All posts by a given user page
-  get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
-
-  # Single post page
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
+  # Define resources for users and their posts
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
