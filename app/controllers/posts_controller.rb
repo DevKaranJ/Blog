c5238e503a62fa36b816d6e4e634821997efc453
Karan Joshi <Karan190213@gmail.com>
2024-01-17 18:21:24 +0530

    wireframe error fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 # app/controllers/posts_controller.rb
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 6)
   end
 
   def show
