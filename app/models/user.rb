8e3656a88e8a8b0668321fd7bcc59c21028e393f
Karan Joshi <Karan190213@gmail.com>
2024-01-18 14:49:38 +0530

    updated user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # app/models/user.rb
 class User < ApplicationRecord
+  has_one_attached :photo
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
